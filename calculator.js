eed80ff80129107803a3d145eb2c6690786356f7
Tramio <tramiodev@outlook.fr>
2022-05-05 19:04:10 +0200

    add more calculator tests (substraction function)

--- calculator.js
+++ calculator.js
@@ -2,11 +2,12 @@ const calculator = ((a, b) => {
   const add = (a, b) => {
     return a + b;
   }
+  const substract = (a, b) => {
+    return a - b;
+  }
   return {
     add,
+    substract,
   }
 })();
-
-const r = calculator.add(2.3, 3.6);
-r
 export default calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -7,3 +7,23 @@ test('adds two integers', () => {
 test('adds two floats', () => {
   expect(calculator.add(2.3, 3.6)).toBeCloseTo(5.9);
 })
+
+test('substracts two integers', () => {
+  expect(calculator.substract(3, 2)).toBe(1);
+})
+
+test('substracts two floats', () => {
+  expect(calculator.substract(3.2, 2.1)).toBeCloseTo(1.1);
+})
+
+test('substracts two negative numbers', () => {
+  expect(calculator.substract(-7, -3)).toBe(-4);
+})
+
+test('substracts a negative number from a positive number', () => {
+  expect(calculator.substract(7, -2)).toBe(9);
+})
+
+test('substracts a positive number from a negative number', () => {
+  expect(calculator.substract(-7, 2)).toBe(-9);
+})
