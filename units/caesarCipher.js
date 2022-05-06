d30b5be63b9cea8af6cd65da9510441672f35051
Tramio <tramiodev@outlook.fr>
2022-05-06 23:56:31 +0200

    refactor caesarCipher to wrap from z to a

--- units/caesarCipher.js
+++ units/caesarCipher.js
@@ -3,8 +3,15 @@ const caesarCipher = (string) => {
   for (let i = 0; i < string.length; i++) {
     indexes.push(string.charCodeAt(i));
   }
-  const ciphered =
-  indexes.map(index => index += 1)
+  const shifted = indexes.map((index) => {
+    if (index == 122) {
+      index = 97;
+    } else {
+      index = index + 1
+    }
+    return index;
+  });
+  const ciphered = shifted
     .map(element => element = String.fromCharCode(element))
     .join("");
   return ciphered;
