a78041b4c279ca4dcc2bef821055276c6e74226a
Tramio <tramiodev@outlook.fr>
2022-05-07 23:28:21 +0200

    pass 1 test

--- units/analyzeArray.js
+++ units/analyzeArray.js
@@ -1,5 +1,19 @@
-const analyzeArray = () => {
-  
+const analyzeArray = (array) => {
+  const getAverage = () => {
+    const sum = array.reduce((a, b) => a + b, 0);
+    const items = array.length;
+    const average = sum / items;
+    return average;
+  }
+  const analyze = () => {
+    let average = getAverage();
+    let result = [average];
+    return result;
+  }
+  const average = getAverage();
+  const result = analyze();
+
+  return average;
 }
 
 export default analyzeArray;
